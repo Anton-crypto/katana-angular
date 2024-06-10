2c3cf6a24a474544b310bd06ba33ad0e03e0e447
Anton-crypto <52647224+Anton-crypto@users.noreply.github.com>
2024-06-11 00:10:00 +0300

    Добавил основу для чата !

--- src/pages/chat/chat.component.ts
+++ src/pages/chat/chat.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, } from '@angular/core';
+import { CommonModule,  } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-page-chat',
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss',
   standalone: true,
+  imports:[CommonModule , FormsModule]
 })
-export class ChatComponent {}
+export class ChatComponent {
+  @Input() message: string = '';
+
+  items: Array<string> = [];
+
+
+  sendMessage(): void {
+    this.items.push(this.message);
+  }
+}
